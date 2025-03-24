23981d406d77706517c6d533239dfb0b25b1bcc6
Christopher Stewart <chriss.stewart4315@gmail.com>
2025-03-24 01:10:50 -0500

    Revert "Finished all of the video that he currently has."
    This reverts commit 910eb28831fa741caa934801e1a5c84a515dfa5a.

--- SmartCafe_Web/Model/Registration.cs
+++ SmartCafe_Web/Model/Registration.cs
@@ -1,15 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace SmartCafe_Web.Model
+﻿namespace SmartCafe_Web.Model
 {
     public class Registration
     {
-        // Declaration - tells razor pages that property named First Name, it will display the first name instead of property name?
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+
     }
 }
