910eb28831fa741caa934801e1a5c84a515dfa5a
Christopher Stewart <chriss.stewart4315@gmail.com>
2025-03-24 01:09:22 -0500

    Finished all of the video that he currently has.

--- SmartCafe_Web/Model/Registration.cs
+++ SmartCafe_Web/Model/Registration.cs
@@ -1,7 +1,15 @@
-﻿namespace SmartCafe_Web.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartCafe_Web.Model
 {
     public class Registration
     {
-
+        // Declaration - tells razor pages that property named First Name, it will display the first name instead of property name?
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
--- SmartCafe_Web/Pages/Account/Register.cshtml.cs
+++ SmartCafe_Web/Pages/Account/Register.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SmartCafe_Web.Model;
 
 namespace SmartCafe_Web.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        public Registration NewUser { get; set; } = new Registration();
         public void OnGet()
         {
+            NewUser.FirstName = "John";
+        }
+
+        // When we submit the form, we POST to the server?
+        public void OnPost()
+        {
+            
         }
     }
 }
