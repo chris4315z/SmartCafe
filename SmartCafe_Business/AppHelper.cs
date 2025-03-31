2372aa6264e435e3effe05d80fb052de7620a912
Lesly Vega <vlesly089@gmail.com>
2025-03-30 23:12:08 -0500

    App Helper & BCrypt finished. Also made SmartCafe_Business a dependency for our web project to work.

--- SmartCafe_Business/AppHelper.cs
+++ SmartCafe_Business/AppHelper.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace SmartCafe_Business
 {
-    internal class AppHelper
+    public class AppHelper
     {
         public static string GetDBConnectionString()
         {
             return "Server=(localdb)\\MSSQLLocalDB;Database=SmartCafe;Trusted_Connection=True;";
+        }
+
+        public static string GeneratePasswordHash(string password)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+            return passwordHash;
         }
     }
 }
