bcd475d717a37e80ebe6bbeaddf4140521b15896
Lesly Vega <vlesly089@gmail.com>
2025-04-23 11:23:11 -0500

    Website is mostly complete. Search, add, edit, delete functions all work. Website is almost completely designed too.

--- SmartCafe_Web/wwwroot/js/site.js
+++ SmartCafe_Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.remove("fade-in");
+
+    document.querySelectorAll("a").forEach(function (link) {
+        if (link.hostname === window.location.hostname && link.getAttribute("href") && !link.getAttribute("href").startsWith("#")) {
+            link.addEventListener("click", function (e) {
+                e.preventDefault();
+                document.body.classList.add("fade-out");
+                setTimeout(() => {
+                    window.location = link.href;
+                }, 600); // match CSS transition time
+            });
+        }
+    });
+});
+
