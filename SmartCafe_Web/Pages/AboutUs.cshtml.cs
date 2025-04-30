57ac5426841dbcc953d56329ec0f4b8b8f8265ba
Lesly Vega <vlesly089@gmail.com>
2025-04-30 00:42:31 -0500

    Adjusted nav-bar to be better.

---
+++ SmartCafe_Web/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartCafe_Web.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
